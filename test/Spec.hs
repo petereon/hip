44f08643f7b479027b3c8b421488a431a1cef8e0
petereon <pvyboch1@gmail.com>
2023-02-20 22:07:13 +0100

    feat: automatic test discovery

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,9 +1,3 @@
-module Main (main) where
-import Test.Hspec
-import Test.QuickCheck
+{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
 
-main :: IO ()
-main = hspec $ do
-  describe "Prelude.head" $ do
-    it "returns the first element of a list" $ do
-      head [23 ..] `shouldBe` (23 :: Int)
+
