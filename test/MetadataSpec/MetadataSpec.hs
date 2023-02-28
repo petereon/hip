896f6ea375873a5e89203824ccc2ac8d8cc60f5b
petereon <pvyboch1@gmail.com>
2023-02-28 14:26:04 +0100

    test: specified rest of the version comparisons

--- test/MetadataSpec/MetadataSpec.hs
+++ test/MetadataSpec/MetadataSpec.hs
@@ -8,6 +8,7 @@ spec = do
   describe "Version.Eq" $ do
     it "compares two equal instances of version" $ do
       Version Nothing [1, 2, 3] Nothing Nothing Nothing Nothing "" == Version Nothing [1, 2, 3] Nothing Nothing Nothing Nothing "check"
+
   describe "Version.Ord" $ do
     it "checks if one version with different patch release is less than another" $ do
       Version Nothing [1, 2, 3] Nothing Nothing Nothing Nothing "" > Version Nothing [1, 2, 2] Nothing Nothing Nothing Nothing ""
@@ -21,3 +22,15 @@ spec = do
       Version (Just 2) [1, 2, 3] Nothing Nothing Nothing Nothing "" > Version (Just 1) [1, 2, 3] Nothing Nothing Nothing Nothing ""
     it "checks if one version with prerelease and another with no prerelease is less than another" $ do
       Version Nothing [1, 2, 3] (Just (Alpha, [1])) Nothing Nothing Nothing "" < Version Nothing [1, 2, 3] Nothing Nothing Nothing Nothing ""
+    it "checks if one version with alpha prerelease is less than another with beta prerelease" $ do
+      Version Nothing [1, 2, 3] (Just (Alpha, [1])) Nothing Nothing Nothing "" < Version Nothing [1, 2, 3] (Just (Beta, [1])) Nothing Nothing Nothing ""
+    it "checks if one version with beta prerelease is less than another with rc prerelease" $ do
+      Version Nothing [1, 2, 3] (Just (Beta, [1])) Nothing Nothing Nothing "" < Version Nothing [1, 2, 3] (Just (Rc, [1])) Nothing Nothing Nothing ""
+    it "checks if one version with rc prerelease is less than another with no prerelease" $ do
+      Version Nothing [1, 2, 3] (Just (Rc, [1])) Nothing Nothing Nothing "" < Version Nothing [1, 2, 3] Nothing Nothing Nothing Nothing ""
+    it "checks if one version with no post release is less than another with post release" $ do
+      Version Nothing [1, 2, 3] Nothing Nothing Nothing Nothing "" < Version Nothing [1, 2, 3] Nothing (Just 1) Nothing Nothing ""
+    it "checks if a version with dev release is less than another with no dev release" $ do
+      Version Nothing [1, 2, 3] Nothing Nothing (Just 1) Nothing "" < Version Nothing [1, 2, 3] Nothing Nothing Nothing Nothing ""
+    it "checks if a version with no local release is less than another with local release" $ do
+      Version Nothing [1, 2, 3] Nothing Nothing Nothing Nothing "" < Version Nothing [1, 2, 3] Nothing Nothing Nothing (Just "local") ""
